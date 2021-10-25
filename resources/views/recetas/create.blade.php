9d74312dfface3f63699466ddc84615e200236a8
marina <marinamarrina@hotmail.com>
2021-10-25 10:59:48 +0200

    registrando routes per memorizare ricette

--- resources/views/recetas/create.blade.php
+++ resources/views/recetas/create.blade.php
@@ -11,7 +11,8 @@
 
 <div class="row justify-content-center mt-5">
     <div class="col-md-8">
-        <form method="POST">
+        <form method="POST" action="{{ route('recetas.store')}}">
+            @csrf
             <div class="form-group">
                 <label for="titulo">Titulo Receta</label>
 
