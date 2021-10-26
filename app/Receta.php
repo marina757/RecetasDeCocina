01753fbae3bb6eae8dbe9307a4eb1e4eabf1b990
marina <marinamarrina@hotmail.com>
2021-10-26 13:39:48 +0200

    creando relazione tra ricette e categorie

--- app/Receta.php
+++ app/Receta.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Receta extends Model
 {
-    //
+    //OBTIENE LA CATEGORIA DE LA RECETA VIA FK
+    public function categoria()
+    {
+        return $this->belongsTo(CategoriaReceta::class);
+    }
 }
