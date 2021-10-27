930303a91ad734f08457df5610c6129cd8992669
marina <marinamarrina@hotmail.com>
2021-10-27 12:56:58 +0200

    creando component vue per la data con momentjs

---
+++ resources/js/components/FechaReceta.vue
@@ -0,0 +1,21 @@
+<template>
+    <span v-text="formatearFecha">
+        
+    </span>
+</template>
+
+<script>
+export default {
+    props: ['fecha'],
+    
+//  mounted() {
+//      console.log('la fecha es ', this.fecha);
+//  },
+ computed: {
+     formatearFecha() {
+         return moment(this.fecha).locale('es').format('DD MMMM YYYY')
+
+     }
+ }
+}
+</script>
