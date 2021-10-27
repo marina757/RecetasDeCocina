7a1e9ba620f9bf23027703f047a97e4aba3eb916
marina <marinamarrina@hotmail.com>
2021-10-27 12:12:34 +0200

    creando view e mostrando info

--- resources/views/recetas/show.blade.php
+++ resources/views/recetas/show.blade.php
@@ -5,5 +5,43 @@
 
 @section('content')
 
-<h1> {{ $receta}}</h1>
+{{--<h1> {{ $receta}}</h1>--}}
+
+<article class="contenido-receta">
+  <h1 class="text-center mb-4">{{$receta->titulo}}</h1>
+  
+  <div class="imagen-receta ">
+     <img src="/storage/{{ $receta->imagen}}" class="w-100">
+  </div>
+
+  <div class="receta-meta mt-2">
+    <p>
+          <span class="font-weight-bold text-primary">Escrito en:</span>
+          {{$receta->categoria->nombre}}
+    </p>
+
+    <p>
+        <span class="font-weight-bold text-primary">Autor:</span>
+        {{--mostrar el usuario --}}
+        {{$receta->user_id}}
+    </p>
+
+    <p>
+        <span class="font-weight-bold text-primary">Fecha:</span>
+        {{$receta->created_at}}
+    </p>
+
+    <div class="ingredientes">
+        <h2 class="my-3 text-primary">Ingredientes</h2>
+
+        {!! $receta->ingredientes !!}{{-- IMPRIME CODIGO HTML--}}
+    </div>
+
+    <div class="preparacion">
+        <h2 class="my-3 text-primary">Preparacion</h2>
+
+        {!! $receta->preparacion !!}{{-- IMPRIME CODIGO HTML--}}
+    </div>
+  </div>
+</article>
 @endsection
