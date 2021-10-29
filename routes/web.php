5a1af2dc201c1d8c16f8010b0f68b349772e7428
marina <marinamarrina@hotmail.com>
2021-10-29 15:22:28 +0200

    toggle per i like

--- routes/web.php
+++ routes/web.php
@@ -35,5 +35,10 @@ Route::get('/perfiles/{perfil}','PerfilController@show')->name('perfiles.show');
 Route::get('/perfiles/{perfil}/edit','PerfilController@edit')->name('perfiles.edit');
 Route::put('/perfiles/{perfil}','PerfilController@update')->name('perfiles.update');
 
+
+//ALMACENA LOS LIKES DE LAS RECETAS
+Route::post('/recetas/${receta}','LikesController@store')->name('likes.store');
+
+
 Auth::routes();
 
