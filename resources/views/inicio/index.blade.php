bb7f0053795f3ab08479a460493b8bbcf6f3ce91
marina <marinamarrina@hotmail.com>
2021-10-30 14:27:59 +0200

    aggiunto owl carousel

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -1,16 +1,15 @@
 @extends('layouts.app')
 
 @section('styles')
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endsection
 
 @section('content')
   <div class="container nuevas-recetas">
-    <h2 class="titulo-categoria text-uppercase mt-5 mb-4">Ultimas Recetas</h2>
+    <h2 class="titulo-categoria text-uppercase mb-4">Ultimas Recetas</h2>
 
-    <div class="row">
+    <div class="owl-carousel owl-theme">
       @foreach($nuevas as $nueva)
-        <div class="col-md-4">
             <div class="card">
                 <img src="/storage/{{ $nueva->imagen}}" class="card-img-top" alt="imagen receta">
 
@@ -22,8 +21,7 @@
                     <a href=" {{ route('recetas.show', ['receta' => $nueva->id]) }}"
                         class="btn btn-primary d-block font-weight-bold text-uppercase">Ver Receta</a>
                 </div>
-            </div>
-        </div>  
+            </div>         
       @endforeach
     </div>
   </div>
