d983458098be5b0e1723b1a4c336dbbd2e86476a
marina <marinamarrina@hotmail.com>
2021-10-30 14:48:53 +0200

    ricette per categoria

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Receta;
+use App\CategoriaReceta;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class InicioController extends Controller
@@ -12,6 +14,19 @@ class InicioController extends Controller
         //OBTENER LAS RECETAS MAS NUEVAS
         $nuevas = Receta::latest()->limit(5)->get();
 
-        return view('inicio.index', compact('nuevas'));
+        //OBTENER TODAS LAS CATEGORIAS
+        $categorias = CategoriaReceta::all();
+        //return $categorias;
+        
+        //AGRUPAR LAS RECETAS POR CATEGORIA
+        $recetas = [];
+
+        foreach ($categorias as $categoria) {
+         $recetas[ Str::slug( $categoria->nombre )] [] = Receta::where('categoria_id', $categoria->id)->take(3)->get();
+        }
+
+        // return $recetas;
+
+        return view('inicio.index', compact('nuevas', 'recetas'));
     }
 }
